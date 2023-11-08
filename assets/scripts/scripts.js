c186f1ee8c4d78cbf2a686fc2cf4ef2e0f3958f3
jaycossey <jaycossey@live.com>
2023-11-08 21:24:10 +0000

    added planning for stucture and functions. readme needs update

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,49 @@
+// GLOBALS --------------------------------------------
+const rootDiv = document.getElementById('root');
+
+// Date Functions -------------------------------------
+/**
+ * This section needs to call and obtain the date
+ * It needs to display at the top of the screen in the 
+ * header tag
+ * 
+ * I need to store and update the date whenever the browser
+ * is open, this needs to be a live view
+ */
+
+
+// Time Functions -------------------------------------
+/**
+ * This section needs to call and obtain the current time
+ * Store this in a variable, update live
+ * 
+ * This will be shown in the header div, and also referenced
+ * inside the list elements, ensuring I change the CSS along
+ * side the updating times
+ * 
+ * The time should be displayed WITH LOCAL TIME!!!
+ * 
+ */
+
+
+// Planner Structure Elements -------------------------
+/**
+ * This should handle the creation and styling of the daily planner
+ * It should have hour long blocks, between 9am-5pm 
+ * 
+ * The li item should display: time block
+ *      input / appointment
+ *      remove / delete appointment button
+ * 
+ * 
+ */
+
+
+// List Element Functions -----------------------------
+/**
+ * This will handle the functionality for each li item
+ * When I click a li time block, then i can input a task
+ * When the time updates, the CSS displays greyed (dark purple) out past
+ *                  mid purple current time
+ *                  light pruple for future time 
+ */
